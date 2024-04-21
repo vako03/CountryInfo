5d587cfc4cfce7daa3679694496d2ac8cd0f4570
Vako <98945500+vako03@users.noreply.github.com>
2024-04-21 14:33:39 +0400

    Initial Commit

---
+++ CountryInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryInfo
+//
+//  Created by vako on 21.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
